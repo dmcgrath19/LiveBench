aac1f70f0357c2dca4898660f0d4dec97043de3d
dmcgrath19 <s2558433@ed.ac.uk>
2024-07-07 18:24:58 +0100

    add script for bench mamba 1.4

--- scripts/mamba-bench-1.4.sh
+++ scripts/mamba-bench-1.4.sh
@@ -35,7 +35,7 @@ conda activate livebench
 # # python show_livebench_results.py    --bench-name live_bench --model-list Mistral-7B-Instruct-v0.2 Llama-2-7b-chat-hf claude-3-opus-20240229
 
 # pip install torch packaging
-pip install -e .
+# pip install -e .
 
 
 
@@ -44,30 +44,13 @@ cd livebench
 echo "Running the livebench script"
 
 # # Generate model answers
-# python gen_model_answer.py --model-path "EleutherAI/pythia-2.8b" --model-id "pythia-2.8b" --dtype bfloat16 
-# python gen_model_answer.py --model-path "EleutherAI/pythia-410m" --model-id "pythia-410m" --dtype bfloat16 
-# python gen_model_answer.py --model-path "EleutherAI/pythia-1b" --model-id "pythia-1b" --dtype bfloat16 
-# python gen_model_answer.py --model-path "EleutherAI/pythia-160m" --model-id "pythia-160m" --dtype bfloat16 
-
-
-# python gen_model_answer.py --bench-name 'live_bench/language' --model-path "state-spaces/mamba-2.8b-hf" --model-id "mamba-2.8b-hf" --dtype bfloat16 
-# python gen_model_answer.py --bench-name 'live_bench/math' --model-path "state-spaces/mamba-2.8b-hf" --model-id "mamba-2.8b-hf" --dtype bfloat16 
-
-# #score the two new ones
-# python gen_ground_truth_judgment.py --bench-name 'live_bench/language'
-# python gen_ground_truth_judgment.py --bench-name 'live_bench/math'
-
-# #update the .csv files of results
-# python show_livebench_results.py
-
-# python gen_ground_truth_judgment.py --bench-name livebench
 
 # python gen_model_answer.py --bench-name 'live_bench/reasoning' --model-path "state-spaces/mamba-2.8b-hf" --model-id "mamba-2.8b-hf" --dtype bfloat16 
 
-# python gen_model_answer.py --model-path "state-spaces/mamba-1.4b-hf" --model-id "mamba-1.4b-hf" --dtype bfloat16 
-# python gen_model_answer.py --model-path "state-spaces/mamba-790m" --model-id "mamba-790m" --dtype bfloat16 
-# python gen_model_answer.py --model-path "state-spaces/mamba-370m" --model-id "mamba-370m" --dtype bfloat16 
-# python gen_model_answer.py --model-path "state-spaces/mamba-130m" --model-id "mamba-130m" --dtype bfloat16 
+python gen_model_answer.py --model-path "state-spaces/mamba-1.4b-hf" --model-id "mamba-1.4b-hf" --dtype bfloat16 
+# python gen_model_answer.py --model-path "state-spaces/mamba-790m-hf" --model-id "mamba-790m-hf" --dtype bfloat16 
+# python gen_model_answer.py --model-path "state-spaces/mamba-370m-hf" --model-id "mamba-370m-hf" --dtype bfloat16 
+# python gen_model_answer.py --model-path "state-spaces/mamba-130m-hf" --model-id "mamba-130m-hf" --dtype bfloat16 
 
 # # python gen_model_answer.py --model-path $MODEL_ID --model-id $MODEL_ID --bench-name $BENCH_NAME
 
