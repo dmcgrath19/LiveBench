b6a3554114b6ed06b5e11fcf9e5e29fd982566bb
dmcgrath19 <s2558433@ed.ac.uk>
2024-07-07 18:15:31 +0100

    make mlp script

---
+++ scripts/mlp-gpu.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=PGR-Standard
+#SBATCH --gres=gpu:a6000:1  # specify the type of GPU you need
+#SBATCH --mem=12000  # memory in Mb
+#SBATCH --time=0-08:00:00
+#SBATCH -o job_output.out  # output file for stdout
+#SBATCH -e job_error.err  # output file for stderr
+
+export HF_HOME="home/s2558433/.cache/huggingface_cache"
+export TRANSFORMERS_CACHE="home/s2558433/.cache/huggingface_cache/transformers"
+export HF_DATASETS_CACHE="home/s2558433/.cache/huggingface_cache/datasets"
+export PIP_CACHE_DIR="home/s2558433/.cache/pip"
+export CONDA_PKGS_DIRS="home/s2558433/.cache/conda_pkgs"
+
+export CXXFLAGS="-std=c99"
+export CFLAGS="-std=c99"
+export TOKENIZERS_PARALLELISM=false
+
+
+source home/s2558433/miniconda3/etc/profile.d/conda.sh
+module load anaconda
+
+cd home/s2558433/LiveBench/
+
+# conda remove env -n livebench
+
+# conda create -n livebench python=3.10
+conda activate livebench
+
+# pip install causal-conv1d>=1.4.0
+# pip install mamba-ssm
+
+# # python gen_model_answer.py          --bench-name live_bench --model-path /path/to/Mistral-7B-Instruct-v0.2/ --model-id Mistral-7B-Instruct-v0.2 --dtype bfloat16 
+# # python gen_api_answer.py            --bench-name live_bench --model gpt-4-turbo
+# # python gen_ground_truth_judgment.py --bench-name live_bench --model-list Mistral-7B-Instruct-v0.2 Llama-2-7b-chat-hf claude-3-opus-20240229
+# # python show_livebench_results.py    --bench-name live_bench --model-list Mistral-7B-Instruct-v0.2 Llama-2-7b-chat-hf claude-3-opus-20240229
+
+# pip install torch packaging
+pip install -e .
+
+
+
+cd livebench
+
+echo "Running the livebench script"
+
+# # Generate model answers
+# python gen_model_answer.py --model-path "EleutherAI/pythia-2.8b" --model-id "pythia-2.8b" --dtype bfloat16 
+# python gen_model_answer.py --model-path "EleutherAI/pythia-410m" --model-id "pythia-410m" --dtype bfloat16 
+# python gen_model_answer.py --model-path "EleutherAI/pythia-1b" --model-id "pythia-1b" --dtype bfloat16 
+# python gen_model_answer.py --model-path "EleutherAI/pythia-160m" --model-id "pythia-160m" --dtype bfloat16 
+
+
+# python gen_model_answer.py --bench-name 'live_bench/language' --model-path "state-spaces/mamba-2.8b-hf" --model-id "mamba-2.8b-hf" --dtype bfloat16 
+# python gen_model_answer.py --bench-name 'live_bench/math' --model-path "state-spaces/mamba-2.8b-hf" --model-id "mamba-2.8b-hf" --dtype bfloat16 
+
+# #score the two new ones
+# python gen_ground_truth_judgment.py --bench-name 'live_bench/language'
+# python gen_ground_truth_judgment.py --bench-name 'live_bench/math'
+
+# #update the .csv files of results
+# python show_livebench_results.py
+
+# python gen_ground_truth_judgment.py --bench-name livebench
+
+# python gen_model_answer.py --bench-name 'live_bench/reasoning' --model-path "state-spaces/mamba-2.8b-hf" --model-id "mamba-2.8b-hf" --dtype bfloat16 
+
+# python gen_model_answer.py --model-path "state-spaces/mamba-1.4b-hf" --model-id "mamba-1.4b-hf" --dtype bfloat16 
+# python gen_model_answer.py --model-path "state-spaces/mamba-790m" --model-id "mamba-790m" --dtype bfloat16 
+# python gen_model_answer.py --model-path "state-spaces/mamba-370m" --model-id "mamba-370m" --dtype bfloat16 
+# python gen_model_answer.py --model-path "state-spaces/mamba-130m" --model-id "mamba-130m" --dtype bfloat16 
+
+# # python gen_model_answer.py --model-path $MODEL_ID --model-id $MODEL_ID --bench-name $BENCH_NAME
+
+# # Score model outputs
+# # python gen_ground_truth_judgment.py --bench-name livebench
+# # Display results
+# python download_questions.py
+
+conda deactivate
