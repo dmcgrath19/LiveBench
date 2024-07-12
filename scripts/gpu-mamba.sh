9d977d8ae673189c01586cbb6adc5a55549dbe56
dmcgrath19 <s2558433@ed.ac.uk>
2024-07-12 09:30:25 +0200

    DM mamba

--- scripts/gpu-mamba.sh
+++ scripts/gpu-mamba.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Use the variable for the job name and log/error files
-#$ -N CR-BENCH-MAMBA
+#$ -N DM-BENCH-MAMBA
 #$ -o /exports/eddie/scratch/s2558433/job_runs/benchMA-1.4_$JOB_ID.log
 #$ -e /exports/eddie/scratch/s2558433/job_runs/benchMA-1.4_$JOB_ID.err
 #$ -cwd
@@ -62,12 +62,13 @@ cd livebench
 # python gen_model_answer.py --bench-name 'live_bench/instruction_following' --model-path "state-spaces/mamba-790m-hf" --model-id "mamba-790m-hf" --dtype bfloat16 
 # python gen_model_answer.py --bench-name 'live_bench/language' --model-path "state-spaces/mamba-790m-hf" --model-id "mamba-790m-hf" --dtype bfloat16 
 
-python gen_model_answer.py --bench-name 'live_bench/coding' --model-path "state-spaces/mamba-790m-hf" --model-id "mamba-790m-hf" --dtype bfloat16
+# python gen_model_answer.py --bench-name 'live_bench/coding' --model-path "state-spaces/mamba-790m-hf" --model-id "mamba-790m-hf" --dtype bfloat16
 
-python gen_model_answer.py --bench-name 'live_bench/reasoning' --model-path "state-spaces/mamba-790m-hf" --model-id "mamba-790m-hf" --dtype bfloat16
+# python gen_model_answer.py --bench-name 'live_bench/reasoning' --model-path "state-spaces/mamba-790m-hf" --model-id "mamba-790m-hf" --dtype bfloat16
 
+python gen_model_answer.py --bench-name 'live_bench/data_analysis' --model-path "state-spaces/mamba-790m-hf" --model-id "mamba-790m-hf" --dtype bfloat16
 
-# python gen_model_answer.py --bench-name 'live_bench/language' --model-path "state-spaces/mamba-790m-hf" --model-id "mamba-790m-hf" --dtype bfloat16 
+python gen_model_answer.py --bench-name 'live_bench/math' --model-path "state-spaces/mamba-790m-hf" --model-id "mamba-790m-hf" --dtype bfloat16 
 
 # python gen_model_answer.py --bench-name 'live_bench/reasoning' --model-path "state-spaces/mamba-1.4b-hf" --model-id "mamba-1.4b-hf" --dtype bfloat16 
 #python gen_model_answer.py --bench-name 'live_bench/code' --model-path "state-spaces/mamba-1.4b-hf" --model-id "mamba-1.4b-hf" --dtype bfloat16 
@@ -76,9 +77,9 @@ python gen_model_answer.py --bench-name 'live_bench/reasoning' --model-path "sta
 #score the two new ones=
 # # python gen_ground_truth_judgment.py --bench-name 'live_bench/coding' --model mamba-1.4b-hf
 # python gen_ground_truth_judgment.py --bench-name 'live_bench/language' --model mamba-1.4b-hf
-# python gen_ground_truth_judgment.py --bench-name 'live_bench/reasoning' --model mamba-1.4b-hf
-python gen_ground_truth_judgment.py --bench-name 'live_bench/instruction_following' --model mamba-790m-hf
-python gen_ground_truth_judgment.py --bench-name 'live_bench/language' --model mamba-790m-hf
+# # python gen_ground_truth_judgment.py --bench-name 'live_bench/reasoning' --model mamba-1.4b-hf
+# python gen_ground_truth_judgment.py --bench-name 'live_bench/instruction_following' --model mamba-790m-hf
+# python gen_ground_truth_judgment.py --bench-name 'live_bench/language' --model mamba-790m-hf
 
 # #update the .csv files of results
 # python show_livebench_results.py
