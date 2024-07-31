1c1565ac1eb4a674cfd0842d7d4378f5e6163670
dmcgrath19 <s2558433@ed.ac.uk>
2024-07-31 09:01:39 +0100

    rerun scripts

--- scripts/gpu-mamba.sh
+++ scripts/gpu-mamba.sh
@@ -78,7 +78,9 @@ cd livebench
 # python gen_model_answer.py --bench-name 'live_bench/math' --model-path "state-spaces/mamba-370m-hf" --model-id "mamba-370m-hf" --dtype bfloat16 
 # python gen_model_answer.py --bench-name 'live_bench/data_analysis' --model-path "state-spaces/mamba-370m-hf" --model-id "mamba-370m-hf" --dtype bfloat16 
 
-python gen_model_answer.py --bench-name 'live_bench/language' --model-path "ArthurZ/mamba-2.8b-slimpj" --model-id "mamba-2.8b-slimpj" --dtype bfloat16 
+python gen_model_answer.py --bench-name 'live_bench/instruction_following' --model-path "ArthurZ/mamba-2.8b-slimpj" --model-id "mamba-2.8b-slimpj" --dtype bfloat16 
+
+python gen_model_answer.py --bench-name 'live_bench/data_analysis' --model-path "ArthurZ/mamba-2.8b-slimpj" --model-id "mamba-2.8b-slimpj" --dtype bfloat16 
 
 # python gen_model_answer.py --bench-name 'live_bench/reasoning' --model-path "ArthurZ/mamba-2.8b-slimpj" --model-id "mamba-2.8b-slimpj" --dtype bfloat16 
 
